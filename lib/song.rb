e4c2a559498536c37f3f0a0ecc19a7afd3f4e327
Brandon Fonnemann <branjosef@gmail.com>
2020-09-18 08:55:10 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+class Song
+  require 'pry'
+
+attr_accessor  :artist, :name
+
+@@all = []
+
+def initialize(name)
+  @name= name
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def self.new_by_filename(filename)
+  #binding.pry
+  artist = filename.split(" - ")[0]
+  name = filename.split(" - ")[1]
+  song = self.new(name)
+  song.artist_name = artist
+  song
+end
+
+
+def artist_name=(name)
+  self.artist = Artist.find_or_create_by_name(name)
+  artist.add_song(self)
+end
+
+end
